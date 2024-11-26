5d16022ed4c8969dd7ad1285ade2570bfd688631
RabayaSultana <rabayashova811@gmail.com>
2024-11-26 21:58:36 +0600

    recipe card added

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,39 @@
+
+const RecipeCard = ({recipe}) => {
+    let recipeIngredients = recipe.ingredients;
+    return (
+        <div className=''>
+        <div className="card  bg-base-100 border ">
+            <figure><img className='w-[330px] h-[200px] p-6 '
+                src={recipe.recipe_image} alt="recipe" /></figure>
+            <div className="card-body">
+                <h2 className="card-title text-xl font-semibold">{recipe.recipe_name}</h2>
+                <p className='text-[#878787] text-xs font-normal'>{recipe.short_description}</p>
+                <hr className='my-3' />
+                <h3 className='text-[#282828] text-xl font-medium'>Ingredients: {recipe.ingredients.length}</h3>
+                <ul className='list-disc ml-10 text-[#878787] text-xs font-normal'>
+                    {
+                        recipeIngredients.map(((ingredient, index) => <li key={index}>{ingredient}</li>))
+                    }
+                </ul>
+                <hr className='my-3' />
+
+                <div className='flex'>
+                    <i className="far fa-clock flex items-center" width="24" height="24"></i>
+                    <p className='text-[16px] text-[#282828] opacity-80 ml-3'>{recipe.preparing_time}</p>
+                    <i className="fas fa-fire-alt flex items-center" width="24" height="24"></i>
+                    <p className='text-[16px] text-[#282828] opacity-80 ml-3'>{recipe.calories}</p>
+                </div>
+
+                <div className="card-actions mt-6">
+                    {/* <button onClick={(e) => handleCookBtn(recipe)} class="btn  cook-btn btn-accent rounded-full">Want to Cook</button> */}
+                    <button>Want to cook</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    );
+};
+
+export default RecipeCard;
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import RecipeCard from "./RecipeCard";
 
 const Recipes = () => {
 
@@ -18,6 +19,18 @@ const Recipes = () => {
             <h2 className="text-[#150B2B] text-[40px] font-semibold mb-6">Our Recipes</h2>
             <p className="text-[16px] font-normal text-[#150B2B]
              opacity-60 mx-5 lg:w-[823px] lg:mx-auto mb-12">Embark on a flavor-filled journey with our thoughtfully curated recipes, designed to delight your palate and ignite your passion for cooking. From zesty appetizers to indulgent desserts, savor the harmony of taste and creativity in every dish.</p>
+        </div>
+
+        {/* result */}
+        <div className='lg:flex lg:mx-10'>
+        <div className='food-container lg:w-3/5 grid lg:grid-cols-2 gap-6'>
+          {/* {
+            foods.map((food) => <Food food={food} handleCookBtn={handleCookBtn}></Food>)
+          } */}
+          {
+            recipes.map((recipe) => <RecipeCard key={recipe.recipe_id} recipe={recipe}></RecipeCard>)
+          }
+        </div>
         </div>
             
         </div>
